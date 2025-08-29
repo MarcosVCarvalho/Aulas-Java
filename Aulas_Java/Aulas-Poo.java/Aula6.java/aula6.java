411ca6fafde20bfdf50a7e0136c0f2c75b6cc477
MarcosVCarvalho <carvalho.marcosvs@gmail.com>
2025-08-29 15:14:58 -0300

    aula6

--- Aulas_Java/Aulas-Poo.java/Aula6.java/aula6.java
+++ Aulas_Java/Aulas-Poo.java/Aula6.java/aula6.java
@@ -1,5 +1,6 @@
 public class aula6 {
     public static void main(String[] args) {
-        
+        String nome = "marcos";
+        System.out.println("meu nome é " + nome);
     }
 }
--- Aulas_Java/Aulas-Poo.java/Aula6.java/controle.java
+++ Aulas_Java/Aulas-Poo.java/Aula6.java/controle.java
@@ -1,5 +1,60 @@
-public class controle {
+public class controle implements interface6 {
     private int volume;
     private boolean ligado;
     private boolean tocando;
+
+    public controle() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+    public int getVolume(){
+        return this.volume;
+    }
+    public void setVolune(int v){
+        this.volume = v;
+    }
+    public boolean getligado(){
+        return this.ligado;
+    }
+    public void setLigado(boolean l){
+        this.ligado = l;
+    }
+    public boolean getTocando(){
+        return this.tocando;
+    }
+    public void setTocando(boolean t){
+        this.tocando = t;
+    }
+    
+    public void ligar(){
+
+    };
+    public void desligar(){
+
+    };
+    public void abrirmenu(){
+
+    };
+    public void fecharmenu(){
+
+    };
+    public void maisvolume(){
+
+    };
+    public void menosvolume(){
+
+    };
+    public void ligarmudo(){
+
+    };
+    public void desligarmudo(){
+
+    };
+    public void play(){
+
+    };
+    public void pause(){
+
+    };
 }
